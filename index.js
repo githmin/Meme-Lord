e33d7779ec925637dce7f4f1287b7d3f5303de2f
Githmin Jayawardhana <githmin21@gmail.com>
2022-08-29 21:15:09 +0530

    changed code to async and await

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const img = document.getElementById('img');
 
 
-document.getElementById('submit').addEventListener('click', UpdateGif);
 
-function UpdateGif(){
-    document.getElementById('loading').textContent = "Okay! Hold up here! Shit takes time to load SARGE!";
-    var input = document.getElementById('inputvalue').value;
-    var fetchUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=RsC2e1JpGuiMsdH354XQJWkgw8EXFNrw&s='+  input ;
+document.getElementById('submit').addEventListener('click', UpdateGif2);
 
-    fetch(fetchUrl , {mode: 'cors'}) .then(function(response){
-        return response.json();
-    }).then(function(response){
-        img.src = response.data.images.original.url ;
-    })
+// function UpdateGif(){
+//     document.getElementById('loading').textContent = "Okay! Hold up here! Shit takes time to load SARGE!";
+//     var fetchUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=RsC2e1JpGuiMsdH354XQJWkgw8EXFNrw&s='+  input ;
 
+//     var input = document.getElementById('inputvalue').value;
+//     fetch(fetchUrl , {mode: 'cors'}) .then(function(response){
+//         return response.json();
+//     }).then(function(response){
+//         img.src = response.data.images.original.url ;
+//     })
+
+// }
+
+
+async function UpdateGif2(){
+    try{
+        document.getElementById('loading').textContent = "Okay! Hold up here! Shit takes time to load SARGE!";
+        var input = document.getElementById('inputvalue').value;
+        var fetchUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=RsC2e1JpGuiMsdH354XQJWkgw8EXFNrw&s='+  input ;
+
+        var response = await fetch(fetchUrl ,  {mode: 'cors'})
+        var encoded = await response.json()
+        img.src = encoded.data.images.original.url
+    } catch( error ){
+        console.log(error)
+    }
 }
